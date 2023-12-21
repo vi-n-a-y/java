2046da02d0d5c3c18373a019c31cf903c8bf6d88
vi-n-a-y <balavinay9063@gmail.com>
2023-12-21 23:14:05 +0530

    pyramid pattern 3

---
+++ PyramidPattern3.java
@@ -0,0 +1,23 @@
+//  *  *  *  *  *  * 
+
+//  *  *  *  *  *
+
+//  *  *  *  *
+
+//  *  *  *
+
+//  *  *
+
+public class PyramidPattern3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 5; j >= i; j--) {
+                System.out.print(" * ");
+            }
+            System.out.println("\n");
+        }
+
+    }
+
+}
