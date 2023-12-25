193d2ff6e535875b2c1d32b851a9166d82ac918d
vi-n-a-y <balavinay9063@gmail.com>
2023-12-25 22:31:18 +0530

    exception throws

---
+++ ExceptionThrows.java
@@ -0,0 +1,9 @@
+public class ExceptionThrows {
+    public static void main(String args[]) throws InterruptedException {
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+    }
+}
