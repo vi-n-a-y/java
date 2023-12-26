cdb1de3fc16538b57a573a48715dbaf70a4e9e72
vi-n-a-y <balavinay9063@gmail.com>
2023-12-26 22:22:13 +0530

    Buzz Number(7)

---
+++ BuzzNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BuzzNumber// a number i.e, end with 7 or divisible by 7.
+{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n : ");
+        int n = sc.nextInt();
+        if (n % 7 == 0 || n % 10 == 7) {
+            System.out.println("Buzz Number");
+        } else {
+            System.out.println("Not a Buzz Number");
+        }
+
+    }
+}
