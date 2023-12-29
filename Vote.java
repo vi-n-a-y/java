afef4b22709b23c35a3e920712ace94f935d9d4c
vi-n-a-y <balavinay9063@gmail.com>
2023-12-29 20:59:48 +0530

    voting age

---
+++ Vote.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Vote {
+    public static void main(String[] args) {
+        int age;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age : ");
+        age = sc.nextInt();
+        if (age >= 18) {
+            System.out.println("You are eligible for vote.");
+        } else {
+            System.out.println("You are not eligible for vote.");
+        }
+    }
+}
