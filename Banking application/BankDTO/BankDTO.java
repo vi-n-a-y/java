d829e160e2cd6d7a7c33d6dcbc7b461af0e80990
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-01-07 23:39:48 +0530

    dto file

---
+++ Banking application/BankDTO/BankDTO.java
@@ -0,0 +1,95 @@
+package com.vin.BankDTO;
+
+
+
+
+public class BankDTO {
+	
+	private String userId;
+	private String uname;
+	private String password;
+	private String fullName;
+	private String email;
+	private String phno;
+	private String address;
+		
+	
+	
+	public BankDTO() {
+		super();
+	}
+
+
+	public String getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+	public String getUname() {
+		return uname;
+	}
+
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPhno() {
+		return phno;
+	}
+
+
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+	
+	
+	
+}
