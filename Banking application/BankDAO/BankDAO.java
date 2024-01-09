d04ac04a2c34c2ea072c90c024bf7bab189d3922
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-01-09 08:11:14 +0530

    database object

--- Banking application/BankDAO/BankDAO.java
+++ Banking application/BankDAO/BankDAO.java
@@ -2,33 +2,50 @@ package com.vin.BankDAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 import com.vin.BankDTO.BankDTO;
 
 
-
+//
 public class BankDAO {
-	public BankDTO getUserDetails(String uname) {
+//	public BankDTO getUserDetails(String uname) {
+//		
+//		BankDTO resUser = null;
+	public int registerEmployee(BankDTO employee)throws ClassNotFoundException{
+		String Insert_user_info="INSERT INTO user_info"+
+				"(userId,uname,password,fullName,email,phno,address)values"+
+				"(?,?,?,?,?,?,?)";
+	
+		int result=0;
 		
-		BankDTO resUser = null;
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank_details","root","root");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank","root","root");
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from user_info");
+//		ResultSet rs = stmt.executeQuery("select * from user_info");
+			PreparedStatement ps=con.prepareStatement(Insert_user_info);
 			
-			while(rs.next())  {
-				System.out.println(rs.getString(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
-			}
-				  
-
+			ps.setInt(1,employee.getUserId());
+			ps.setString(1,employee.getUname());
+			ps.setString(1,employee.getPassword());
+			ps.setString(1,employee.getFullName());
+		    ps.setString(1,employee.getEmail());
+			ps.setString(1,employee.getPhno());
+			ps.setString(1,employee.getAddress());
+			
+			
+			result=ps.executeUpdate();
+	
 		}catch(Exception ex) {
 			ex.printStackTrace();
 		}
 		
-		return resUser;
+		return result;
 	}
+	
+	
 }
