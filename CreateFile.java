644f9e438f2d3d6434bd9418281a3a7f7287470f
vi-n-a-y <balavinay9063@gmail.com>
2024-01-10 21:26:20 +0530

    file information

--- CreateFile.java
+++ CreateFile.java
@@ -7,7 +7,13 @@ public class CreateFile {
         if (f.createNewFile()) {
             System.out.println("file succesfull created");
         } else if (f.exists()) {
-            System.out.println("already file is exists");
+            if (f.exists()) {
+                System.out.println("The name of the file is " + f.getName());
+                System.out.println("It  is readable file " + f.canRead());
+                System.out.println("It is writable file  " + f.canWrite());
+                System.out.println("Get the absolute file locaton" + f.getAbsolutePath());
+                System.out.println("The length of the file is " + f.length());
+            }
         } else {
             System.out.println("external error while creating the file ");
         }
