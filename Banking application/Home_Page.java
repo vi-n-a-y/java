60c2b7c0f4f882e3286afa65efe59902af2965d3
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-01-12 23:56:45 +0530

    changes in home

--- Banking application/Home_Page.java
+++ Banking application/Home_Page.java
@@ -1,7 +1,7 @@
 //import jakarta.servlet.RequestDispatcher;
 
 import jakarta.servlet.ServletException;
-//import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 
 
 import java.io.PrintWriter;
-//@WebServlet("/home")
+@WebServlet("/home")
 public class Home_Page extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
@@ -46,7 +46,7 @@ public class Home_Page extends HttpServlet {
 		
 
 		try {
-			String again= "<font color=\"red\">login failed , Try again!</font>\r\n";
+	
 			
 			Class .forName("com.mysql.cj.jdbc.Driver");
 			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bank","root","root");
@@ -61,75 +61,17 @@ public class Home_Page extends HttpServlet {
 				
 			}
 			else {
-				response.getWriter().append("<html><body>").append(again).append("</body></html>");
-				
-				response.sendRedirect("http://localhost:8080/Banking_application/bank.html");
+				out.println("login failed");
+
 				
 			}
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 				
-//	String res="<html>\r\n"
-//			+ "<body>\r\n"
-//			+ "<div>\r\n"
-//			+"<button style=\"float:right;\">logout</button>"
-//			+ "<h1 color=\"blue\" align=\"center\">Home page</h1>\r\n"			
-//			+ "</div>\r\n"
-//			+ "<form>\r\n"
-//			+ "<label>user Info :</label>\r\n"
-//			+ "<input type=\"text\" id=\"info\"  value="+request.getParameter("u_name")+" name=\"uinfo\"><br><br>\r\n"
-//			+ "<label>full_name :</label>\r\n"
-//			+ "<input type=\"text\" id=\"fn\" value="+request.getParameter("u_name")+"michel"+" name=\"fullname\"><br><br>\r\n"
-//			+ "<label>phone :</label>\r\n"
-//			+ "<input type=\"text\" id=\"ph\" name=\"ph\" value="+x+"><br><br>\r\n"
-//			+ "<label>email :</label>\r\n"
-//			+ "<input type=\"text\" id=\"email\" value="+request.getParameter("u_name")+"@gmail.com"+" name=\"email\"><br><br>\r\n"
-//			+ "\r\n"
-//			+ "<div>\r\n"
-//			+ "<h3 color=\"red\">Accounts: </h3>\r\n"
-//			+ "<input type=\"radio\" ><label>Account Number</label>\r\n"
-//			+ "<input type=\"text\" id=\"acc_no\" name=\"acc_no\" value="+y+"><br><br>\r\n"
-//			+ "<label>IFSC :</label>\r\n"
-//			+ "<input type=text id=\"ifsc\" name=\"ifsc\"><br><br>\r\n"
-//			+ "<label>Account_type :</label>\r\n"
-//			+ "<input type=text id=\"acc_type\" name=\"acc_type\" value="+"SAVINGS"+"><br><br>\r\n"
-//			+ "<label>current_balance :</label>\r\n"
-//			+ "<input type=text id=\"curr_bal\" name=\"curr_bal\"><br><br>\r\n"
-//			+ "<label>last transaction date :</label>\r\n"
-//			+ "<input type=text id=\"lst_txn_dt\" name=\"lst_txn_dt\"><br><br>\r\n"
-//			+ "</div>\r\n"
-//			+ "<div>\r\n"
-//			+ "<label>Statement request form :</label>\r\n"
-//			+ "<button onclick=\"stm()\">press here</button>\r\n"
-//			+ "</div>\r\n"
-//			+ "</form>\r\n"
-//			+ "</body>\r\n"
-//			+ "</html>";
-//		response.getWriter().append(res);
-//		
-//
-//	response.sendRedirect("Stmt");
-		
-		
-		
-		
-		
-//		
-//	}
-//
-//	private void registerEmployee(BankDTO employee) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	private String getParameter(String string) {
-//		// TODO Auto-generated method stub
-//		return null;
+
 	}
 }
 
