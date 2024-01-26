b0fa9f54a5a54f17ea7cceca7e1322d5e2dbb8fe
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-01-26 23:54:28 +0530

    add balance to the account

--- Banking application/AddMoney.java
+++ Banking application/AddMoney.java
@@ -5,36 +5,50 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class AddMoney
- */
+import java.io.IOException;
+import com.vin.bankdao.BankDAO;
+import com.vin.bankdto.BankDTO;
+
+@WebServlet("/add")
+
 public class AddMoney extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public AddMoney() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	doPost(request,response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session=request.getSession();	
+		BankDAO addDao = new  BankDAO();
+		BankDTO user = new  BankDTO();
+		
+		user=(BankDTO) session.getAttribute("user");
+		int id=user.getUserId();
+		System.out.println(id);
+		
+		
+		double amount=Double.parseDouble(request.getParameter("add_amt"));
+		
+		addDao.updateCurrBal(amount, id);
+		System.out.println(addDao);
+		if(addDao!=null) {
+			response.sendRedirect("Statement.jsp");
+		}
+		else {
+			response.sendRedirect("bank.jsp");
+		}
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
