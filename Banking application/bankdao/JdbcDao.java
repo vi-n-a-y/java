9caebd73bf94d3cfdf5b25f88dc02ebc711a5709
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-01-28 23:53:08 +0530

    jdbc connector

---
+++ Banking application/bankdao/JdbcDao.java
@@ -0,0 +1,29 @@
+package com.vin.bankdao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcDao {
+	
+	private static String jdbcURL = "jdbc:mysql://localhost:3306/bank";
+	private static String jdbcUsername = "root";
+	private static String jdbcPassword = "root";
+
+	public static Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+
+}
