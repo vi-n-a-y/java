c2e6ed222f81785ef4e8422e0091fde3d4520558
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-01-31 23:49:55 +0530

    changes in jsp

--- Banking application/src/main/java /Home_Page.java
+++ Banking application/src/main/java /Home_Page.java
@@ -11,6 +11,8 @@ import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 
 import java.lang.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.vin.bankdto.AccountDTO;
 import com.vin.bankdto.BankDTO;
@@ -21,66 +23,33 @@ import java.io.PrintWriter;
 public class Home_Page extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	  BankDAO detailsDao=new BankDAO();
-	  BankDTO details_dto =new BankDTO();
-	  
-	  
-	
-	  
-	  
-	  
+	  BankDTO details_dto =new BankDTO();  
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request,response);
-		
-		
-
-	
-	
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
-		PrintWriter pw= response.getWriter();
-
-		
-		
-
-
 		HttpSession session = request.getSession();
 
-	RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
+		
 			String user_name=request.getParameter("u_name");
 			String user_pass= request.getParameter("u_pass");
 			BankDTO  user=detailsDao.getUserDetails(user_name);
-
-			
-
-			if(user!=null) {
-				
+			if(user!=null) {			
 				if (user.getPassword().equals(user_pass)) {
 					session.setAttribute("user", user);
-//					rd.forward(request, response);
-					int id=user.getUserId();
-					
-					 System.out.println(id);
-					 AccountDTO  data=detailsDao.getAccDetails(id);
-//					 if(data!=null) {
-							session.setAttribute("data", data);
+					int id=user.getUserId();	
+					 System.out.println("sysout in Home_page java "+id);
+//					 List<AccountDTO>  data=detailsDao.getAccDetails(id);
+//							session.setAttribute("data", data);
+							RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
 							rd.forward(request, response);
-				}
+							
+							
+				} 
+				else {
+					throw new RuntimeException("No Account found,Enter correct Crenditials to Login!");
+	}
 			}
-			else {
-				response.sendRedirect("addMoney.jsp");
-			
-			}
-				
-
-				 
-				
-				
-			
-			
-	
-
 		
 		
 }
