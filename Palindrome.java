ca2b31068616d16cba61baebaf67763d3030ea46
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-03-27 23:36:58 +0530

    check Palindrome number

--- Palindrome.java
+++ Palindrome.java
@@ -1,17 +1,20 @@
 public class Palindrome {
     // if we reverse a number same number appears.
-    public static void main(String args[]) {
-        int rev = 0;
-        int a = 1221;
-        int temp = a;
-        while (a > 0) {
-            int res = a % 10;
-            rev = rev * 10 + res;
-            a = a / 10;
-        }
-        if (rev == temp)
-            System.out.println("palindrome number " + rev);
-        else
-            System.out.println("not a palindrome number");
-    }
+  public static void main(String[] args) {
+		int num=11211;
+		int rev=0,rem=0;
+		int temp=num;
+		while(num!=0) {
+			rem=num%10;
+			rev=rev*10+rem;
+			num=num/10;
+		}
+		System.out.println(rev);
+		if(rev==temp) {
+			System.out.println("the number is palindrom number");
+		}else {
+			System.out.println("not a palindrom number");
+		}
+
+	}
 }
