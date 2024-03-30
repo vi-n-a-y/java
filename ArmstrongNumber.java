b7430792df36975d61acad9f8f64b1b86f8252b5
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-03-30 23:58:28 +0530

    Update ArmstrongNumber.java

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -3,11 +3,7 @@ public class ArmstrongNumber {
         int n = 371;
         int rem, num = 0;
         int temp = n;
-        while (n > 0) {
-            rem = n % 10;
-            num = num + (rem * rem * rem);
-            n = n / 10;
-        }
+       
         if (num == temp) {
             System.out.println(num + " is  a Armstrong Number ");
         } else
