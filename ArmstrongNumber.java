1bb6fd8ef499970da6e6772c1171d31382904c9f
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-03-31 22:56:42 +0530

    ArmstrongNumber

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,12 +1,35 @@
-public class ArmstrongNumber {
-    public static void main(String args[]) {
-        int n = 371;
-        int rem, num = 0;
-        int temp = n;
-       
-        if (num == temp) {
-            System.out.println(num + " is  a Armstrong Number ");
-        } else
-            System.out.println("The Number is not a Armstrong Number!");
-    }
+public class AmstrongNumber {
+
+	public static int amstrong(int num) {
+		int digit = 0;
+		while (num > 0) {
+
+			num = num / 10;
+			digit++;
+
+		}
+
+		return digit;
+	}
+
+	public static void main(String[] args) {
+		int num = 153;
+
+		int temp = num;
+		int rem, rev = 0;
+		int digit = amstrong(num);
+		while (num > 0) {
+			rem = num % 10;
+			rev = (int) (rev + (Math.pow(rem, digit)));
+			num /= 10;
+		}
+
+		if (rev == temp) {
+			System.out.println("Given number is amstrong number");
+		} else
+			System.out.println("Not a amstrong number");
+
+	}
+
 }
+
