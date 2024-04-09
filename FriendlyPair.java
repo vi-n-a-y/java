f68b6d6f826edd478478f3faab57b3a15a20ff4a
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-04-09 23:32:57 +0530

    Add files via upload

---
+++ FriendlyPair.java
@@ -0,0 +1,31 @@
+package com.vin;
+
+public class FriendlyPair {
+	public static int friendPair(int num) {
+		int sum=0;
+		for(int i=1;i<=num/2;i++) {
+			if(num%i==0) {
+				sum=sum+i;
+				System.out.println(i);
+			}
+			
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int num1=6;
+		int num2=28;
+		int sum1=friendPair(num1);
+		int sum2=friendPair(num2);
+		if(sum1/num1==sum2/num2) {
+			System.out.println("Friendly Pair Numbers");
+		}else {
+			System.out.println("Not a Friendly Numbers");
+		}
+		
+		
+
+	}
+
+}
