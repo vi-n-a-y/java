f76370ebfe1551a414a548cb89a89bcfd2ae967f
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-04-13 21:36:24 +0530

    changes in ElectricityBill.java

--- ElectricityBill.java
+++ ElectricityBill.java
@@ -1,21 +1,27 @@
+package com.vin;
+
+import java.util.Scanner;
+
 public class ElectricityBill {
-    public static void main(String args[]) {
-        int units = 144;
-        int bill = 0;
 
-        if (units > 100) {
-            if (units >= 200) {
-                if (units > 300) {
-                    bill = units * 8;
-                } else
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int units = sc.nextInt();
 
-                    bill = units * 6;
-            } else
-                bill = units * 5;
-        }
+		int amount = 0;
+		if (units > 100 && units < 200) {
+			amount = units * 6;
+		} else if (units > 200 && units < 300) {
+			amount = units * 7;
+		} else if (units > 300) {
+			amount = units * 8;
+		} else {
+			amount = units * 5;
+		}
 
-        System.out.println("Eastern power distribution : ");
-        System.out.println("Units Consumed : " + units);
-        System.out.println("Total Bill : " + bill);
-    }
+		System.out.println("The total Electricity amount is :" + amount);
+
+	}
+
 }
+
