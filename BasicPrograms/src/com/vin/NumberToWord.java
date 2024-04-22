a109f5d5d989dcbaf021cc7b6e22ca2fc1bba429
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-04-22 16:56:04 +0530

    Update NumberToWord.java

--- BasicPrograms/src/com/vin/NumberToWord.java
+++ BasicPrograms/src/com/vin/NumberToWord.java
@@ -5,43 +5,78 @@ import java.util.Scanner;
 public class NumberToWord {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the number : ");
-		int n=sc.nextInt();
-		
-		switch(n) {
-		case 1:if(n==1) 
-			System.out.print("one");
-		
-		break;
-		case 2: if(n==2)
-			System.out.print("two");
-		break;
-		case 3 : if(n==3)
-			System.out.print("three");
-		break;
-		case 4 : if(n==4)
-			System.out.println("four");
-		break;
-		case 5 : if(n==5)
-			System.out.print("five");
-		break;
-		case 6: if(n==6)
-			System.out.print("six");
-		break;
-		case 7 : if(n==7)
-			System.out.print("seven");
-		break;
-		case 8 : if(n==8)
-			System.out.println("Eight");
-		break;
-		case 9 : if(n==9)
-			System.out.print("Nine");
-		break;
-		case 10 : if(n==0)
-			System.out.print("Zer0");
-		break;
-		default : System.out.println("wrong input");
-	}
+		int n = sc.nextInt();
+		int rem1 = 0, rev1 = 0, count = 0;
+		while (n > 0) {
+			rem1 = n % 10;
 
+			rev1 = rev1 * 10 + rem1;
+			n /= 10;
+			if (rev1 == 0) {
+				count++;
+			}
+
+		}
+
+		int rem = 0;
+
+		while (rev1 > 0) {
+			rem = rev1 % 10;
+
+			switch (rem) {
+
+			case 0:
+				System.out.print("zero ");
+				break;
+			case 1:
+
+				System.out.print("one ");
+
+				break;
+			case 2:
+
+				System.out.print("two ");
+				break;
+			case 3:
+
+				System.out.print("three ");
+				break;
+			case 4:
+
+				System.out.print("four ");
+				break;
+			case 5:
+
+				System.out.print("five ");
+				break;
+			case 6:
+
+				System.out.print("six ");
+				break;
+			case 7:
+
+				System.out.print("seven ");
+				break;
+			case 8:
+
+				System.out.print("Eight ");
+				break;
+			case 9:
+
+				System.out.print("Nine ");
+				break;
+
+			default:
+				System.out.print("wrong input");
+			}
+			rev1 /= 10;
+
+		}
+		for (int i = 0; i < count; i++) {
+			System.out.print("zero ");
+		}
+
+	}
 }
