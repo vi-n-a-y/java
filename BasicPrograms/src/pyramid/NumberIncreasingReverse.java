2978503b6a9e87680d8ec4dc112f6b6a3cd5f5d7
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-05-08 17:01:44 +0530

    Add files via upload

---
+++ BasicPrograms/src/pyramid/NumberIncreasingReverse.java
@@ -0,0 +1,19 @@
+package pyramid;
+
+public class NumberIncreasingReverse {
+
+	public static void main(String[] args) {
+		for(int i=4;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+//1 2 3 4 
+//1 2 3 
+//1 2 
+//1 
---
+++ BasicPrograms/src/pyramid/ZeroOneTriangle.java
@@ -0,0 +1,25 @@
+package pyramid;
+
+public class ZeroOneTriangle {
+
+	public static void main(String[] args) {
+	for(int i=0;i<5;i++) {
+		for(int j=0;j<=i;j++) {
+			if((i+j)%2==0) {
+				System.out.print("1 ");
+			}else {
+				System.out.print("0 ");
+			}
+	}
+		System.out.println();
+
+	}
+	}
+
+}
+
+//1 
+//0 1 
+//1 0 1 
+//0 1 0 1 
+//1 0 1 0 1 
