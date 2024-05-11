ae478160facd24a69038020c3bf74f1aae069e24
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-05-11 21:45:35 +0530

    Update BinarySearchRecursion.java

--- BasicPrograms/src/com/vin/BinarySearchRecursion.java
+++ BasicPrograms/src/com/vin/BinarySearchRecursion.java
@@ -19,7 +19,7 @@ public class BinarySearchRecursion {
 	}
 
 	public static void main(String args[]) {
-		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int arr[] = { 6,7,8,9,10,11,12,13};
 		int n = usingRecursion(arr, 0, arr.length - 1, 8);
 		if (n >= 0) {
 			System.out.println("element found at the index of : " + n);
