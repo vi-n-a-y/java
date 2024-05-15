6126589cdd098d213518666472a72ba8e5bc5ef7
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-05-15 23:59:57 +0530

    Add files via upload

---
+++ BasicPrograms/src/pyramid/StarReversePattern.java
@@ -0,0 +1,18 @@
+package com.vin;
+
+public class StarReversePattern {
+
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<5;j++) {
+				System.out.print(" ");
+				for(int k=5;k>0;k++) {
+					System.out.print(" * ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
