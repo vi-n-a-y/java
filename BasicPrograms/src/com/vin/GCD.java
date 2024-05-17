65031cdc73b1ede97279adfe2def490306b0dc41
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-05-17 13:00:06 +0530

    Add files via upload

---
+++ BasicPrograms/src/com/vin/GCD.java
@@ -0,0 +1,25 @@
+package com.vin;
+
+public class GCD {
+
+	public static int findGCD(int n,int n2) {
+		
+		if(n>n2) {
+			n=n*n2;
+			n2=n/n2;
+			n=n/n2;
+		}
+		int gcd=0;
+	
+		for(int i=1;i<n2;i++) {
+			if(n%i==0 && n2%i==0) {
+				gcd=i;
+			}
+		}
+		return gcd;
+	}
+	public static void main(String args[]) {
+		int g=findGCD(48,36);
+		System.out.println(g);
+	}
+}
