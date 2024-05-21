27d26349953c6a0ebca7db597bd039ec8f4a8277
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-05-22 00:10:16 +0530

    Update StarReversePattern.java

--- BasicPrograms/src/pyramid/StarReversePattern.java
+++ BasicPrograms/src/pyramid/StarReversePattern.java
@@ -4,12 +4,13 @@ public class StarReversePattern {
 
 	public static void main(String[] args) {
 		for(int i=0;i<5;i++) {
-			for(int j=i;j<=5;j++) {
+			for(int j=0;j<=i;j++) {
 				System.out.print(" ");
-				for(int k=5;k>0;k--) {
+			}
+				for(int k=5;k>i;k--) {
 					System.out.print(" * ");
 				}
-			}
+			
 			System.out.println();
 		}
 
