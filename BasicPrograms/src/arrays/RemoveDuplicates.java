4ad13600db2d78defadd3b42b1a3e3e0720ee045
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-05-21 23:00:19 +0530

    Update RemoveDuplicates.java

--- BasicPrograms/src/arrays/RemoveDuplicates.java
+++ BasicPrograms/src/arrays/RemoveDuplicates.java
@@ -7,6 +7,7 @@ public class RemoveDuplicates {
 
 		int i = 0;
 		int n=arr.length();
+		boolean isduplicate =false;
 		for (int j = 1; j < n; j++) {
 			if (arr[i] != arr[j]) {
 				arr[i + 1] = arr[j];
